5b8ff636ecda7fa104bc004ab9f8501199846508
gggeek <ggiunta@kaliop.com>
2015-09-23 00:41:33 +0200

    2nd travis fix

---
+++ Tests/travis/setup_broker.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+case "$1" in
+rabbitmq)
+    # rabbitmq is already installed and started
+    ;;
+apollo)
+    wget http://www.apache.org/dyn/closer.cgi?path=activemq/activemq-apollo/1.7.1/apache-apollo-1.7.1-unix-distro.tar.gz
+    tar -zxvf apache-apollo-1.7.1-unix-distro.tar.gz
+    cd apache-apollo-1.7.1
+    ./bin/apollo create testbroker
+    ./testbroker/bin/apollo-broker run &
+    ;;
+activemq)
+    echo NOT YET SUPPORTED
+    ;;
+*)
+    echo unknown broker: $1
+    ;;
+esac
