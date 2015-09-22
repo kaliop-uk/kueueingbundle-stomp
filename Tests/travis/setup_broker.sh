b56c3ca9aa465e0e13c9a4de8f13332d4b173d9e
gggeek <ggiunta@kaliop.com>
2015-09-23 01:01:57 +0200

    travis fix 7

--- Tests/travis/setup_broker.sh
+++ Tests/travis/setup_broker.sh
@@ -5,7 +5,7 @@ rabbitmq)
     # rabbitmq is already installed and started
     ;;
 apollo)
-    wget -O apache-apollo-1.7.1-unix-distro.tar.gz http://www.apache.org/dyn/closer.cgi?path=activemq/activemq-apollo/1.7.1/apache-apollo-1.7.1-unix-distro.tar.gz
+    wget http://www.eu.apache.org/dist/activemq/activemq-apollo/1.7.1/apache-apollo-1.7.1-unix-distro.tar.gz
     tar -zxvf apache-apollo-1.7.1-unix-distro.tar.gz
     cd apache-apollo-1.7.1
     ./bin/apollo create testbroker
