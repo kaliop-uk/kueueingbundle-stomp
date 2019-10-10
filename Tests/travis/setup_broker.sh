00e93cfd6d284789e974c86c68f19fafa44d0406
gggeek <ggiunta@kaliop.com>
2019-10-10 07:42:11 +0000

    travis

--- Tests/travis/setup_broker.sh
+++ Tests/travis/setup_broker.sh
@@ -11,17 +11,24 @@ echo "Setting up broker: $1..."
 case "$1" in
 
 activemq)
+    # Ubuntu package
     sudo apt-get install -y activemq
     sudo cp ./Tests/travis/activemq.xml /etc/activemq/instances-available/main/activemq.xml
     sudo ln -s /etc/activemq/instances-available/main /etc/activemq/instances-enabled/
     sudo service activemq restart
+    # Alternative: latest version as tarball
+    #wget https://archive.apache.org/dist/activemq/5.15.9/apache-activemq-5.15.9-bin.tar.gz
+    #tar -zxvf apache-activemq-5.15.9-bin.tar.gz
+    #mv apache-activemq-5.15.9 apache-activemq
+    #cd apache-activemq
+    #bin/activemq start >/dev/null  2>&1 &
     ;;
 
 apollo)
     # NB: Apollo is 'dead' since march 2019... the lat available build is 1.7.1.
     # It does not work on JRE 11, so we install version 8
     sudo apt-get install -y openjdk-8-jdk-headless
-    sudo  update-java-alternatives --set java-1.8.0-openjdk-amd64
+    sudo  update-java-alternatives -v --jre-headless --set java-1.8.0-openjdk-amd64
     wget http://archive.apache.org/dist/activemq/activemq-apollo/1.7.1/apache-apollo-1.7.1-unix-distro.tar.gz
     tar -zxvf apache-apollo-1.7.1-unix-distro.tar.gz
     mv apache-apollo-1.7.1 apache-apollo
